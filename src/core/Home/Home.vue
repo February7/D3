6b2297c9ab8db40837d03fb4e6c34e8a3d884a0f
weiyapeng <yapeng.wei@dclingcloud.com>
2022-03-10 18:52:28 +0800

    修改命名问题

---
+++ src/core/Home/Home.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref, getCurrentInstance } from "vue";
+import { useRouter } from "vue-router";
+import {
+  logoSvg,
+  dashboardSvg,
+  jobsSvg,
+  applicationSvg,
+  teamSvg,
+  reportSvg,
+} from "./svg";
+
+const inst = getCurrentInstance();
+const router = useRouter();
+
+const navActiveIndex = ref(0);
+
+const navList = [
+  { name: "No.1 入门", svg: dashboardSvg, href: "/No.1" },
+  { name: "No.2", svg: jobsSvg, href: "#" },
+  { name: "No.3", svg: applicationSvg, href: "#" },
+  { name: "No.4", svg: teamSvg, href: "#" },
+  { name: "No.5", svg: reportSvg, href: "#" },
+];
+
+const goHome = () => {
+  router.push('/');
+}
+
+const jump = (el, href, i) => {
+  el.stopPropagation();
+
+  router.push(href);
+  navActiveIndex.value = i;
+};
+
+inst.render = () => (
+  <>
+    <div class="app-container">
+      <div class="app-left">
+        <div class="app-logo" onClick={() => goHome()}>
+          {logoSvg}
+          <span>D3.js</span>
+        </div>
+        <ul class="nav-list">
+          {navList.map((e, i) => (
+            <li
+              class={["nav-list-item", navActiveIndex.value === i && "active"]}
+            >
+              <a class="nav-list-link" onClick={(el) => jump(el, e.href, i)}>
+                {e.svg}
+                {e.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div class="app-main">
+        <RouterView></RouterView>
+      </div>
+    </div>
+  </>
+);
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
---
+++ src/core/Home/svg.js
@@ -0,0 +1,109 @@
+export const logoSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-bar-chart-2"
+  >
+    <line x1="18" y1="20" x2="18" y2="10" />
+    <line x1="12" y1="20" x2="12" y2="4" />
+    <line x1="6" y1="20" x2="6" y2="14" />{" "}
+  </svg>
+);
+
+export const dashboardSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-columns"
+  >
+    <path d="M12 3h7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-7m0-18H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7m0-18v18" />
+  </svg>
+);
+
+export const jobsSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-briefcase"
+  >
+    <rect x="2" y="7" width="20" height="14" rx="2" ry="2" />
+    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16" />
+  </svg>
+);
+
+export const applicationSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-file"
+  >
+    <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
+    <polyline points="13 2 13 9 20 9" />
+  </svg>
+);
+
+export const teamSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-users"
+  >
+    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+    <circle cx="9" cy="7" r="4" />
+    <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
+    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+  </svg>
+);
+
+export const reportSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="feather feather-pie-chart"
+  >
+    <path d="M21.21 15.89A10 10 0 1 1 8 2.83" />
+    <path d="M22 12A10 10 0 0 0 12 2v10z" />
+  </svg>
+);
